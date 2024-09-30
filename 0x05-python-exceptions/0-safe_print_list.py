e7dbf6855459b51c112bcf5a78175545ad047735
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-09-30 14:16:46 +0300

    Integers division with debug

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for item in range(x):
             print("{}".forma(my_list[item], end='')
-            num: += 1
+            num += 1
     except IndexError:
         pass
     print()
