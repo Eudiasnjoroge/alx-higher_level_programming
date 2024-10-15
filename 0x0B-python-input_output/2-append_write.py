fa472f606a16f7b255b4ebd2c4c86e45a44d69a9
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-10-15 23:22:51 +0300

    Correction of append-write file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
+"""defining append_write functions"""
+
 
 def append_write(filename="", text=""):
-    """Appends a string at the end of a text file and returns the number of characters added.
-    Args:
-        filename (str): The name ofthe file to append to. If the file does not exist, it will be created.
-        text (str): The string to append to the file.
-
-    Returns:
-        int: The number of characters added.
-    """
-    with open(filename, "a", encoding="utf-8") as file:
-        return file.write(text)
+    """appends filename with utf-8"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
