1abaf2f1258533c2c536db67b680dcbbb0a95972
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-11-21 22:00:40 +0300

    Readme

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- script to list all privileges for users user_0d_1 and user_0d_2
+-- users must exist and have privileges assigned before executing this script
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
