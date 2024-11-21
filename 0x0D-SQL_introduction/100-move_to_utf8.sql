643c386c3349149f9459ee16ec3f5bcabde87d2c
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-11-21 21:54:43 +0300

    UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,17 +1,11 @@
--- Convert the database hbtn_0c_0 to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
--- Convert table first_table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
--- Convert field name in first_table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+-- This script converts the `hbtn_0c_0` database, `first_table` table, and `name` field to UTF8 (utf8mb4).
+-- The collation will be set to utf8mb4_unicode_ci for better compatibility.
 
--- Change the database character set and collation
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+-- Convert the database to utf8mb4
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
--- Change the table first_table character set and collation
-ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+-- Convert the `first_table` table to utf8mb4
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Change the column 'name' in first_table to UTF8 (utf8mb4, collation utf8mb4_unicode_ci)
-ALTER TABLE first_table
-MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the `name` field in `first_table` to utf8mb4
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
