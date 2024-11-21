6de279f40b55bf1f61d8152ff47d72c5ba6f427f
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-11-22 00:15:05 +0300

    identify genre id for comedy

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,14 @@
+-- Step 1: Identify the genre_id for Comedy
+-- Step 2: Find all shows that are not linked to the Comedy genre
+SELECT title
+FROM tv_shows
+WHERE id NOT IN (
+	SELECT show_id
+	FROM tv_show_genres
+	WHERE genre_id = (
+		SELECT id
+		FROM tv_genres
+		WHERE name = 'Comedy'
+	)
+)
+ORDER BY title ASC;
