ca878142e48116a0f83ecd90a4c6730e68c5bf14
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2025-01-17 22:44:08 +0300

    2-post_email.py correction

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,9 +10,10 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    email = sys.argv[2]
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(response) as response:
-        print(response.read().decode("utf-8")
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read().decode("utf-8")
+        print(body)
