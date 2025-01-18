3ce7b10d5e8130147458d435ed33d6d1f7435721
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2025-01-18 12:55:59 +0300

    task 9 correction

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,11 +9,11 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     token = sys.argv[2]
-    
+
     url = "https://api.github.com/user"
-    
+
     response = requests.get(url, auth=(username, token))
-    
+
     if response.status_code == 200:
         print(response.json().get("id"))
     else:
