fb190536a75ae115e87a0e27baf83d36e0963b41
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2025-01-18 12:54:58 +0300

    task 8 correction

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,14 +8,14 @@ import sys
 
 if __name__ == "__main__":
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    
+
     data = {"q": letter}
-    
+
     try:
         response = requests.post("http://0.0.0.0:5000/search_user", data=data)
-        
+
         response_json = response.json()
-        
+
         if response_json:
             print(f"[{response_json.get('id')}] {response_json.get('name')}")
         else:
