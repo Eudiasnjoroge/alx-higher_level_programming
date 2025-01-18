5e9e671c8ab080e3de65ada755ab3fbcb416453e
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2025-01-18 12:38:39 +0300

    task 4 correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,12 +7,11 @@ import requests
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    
+
     response = requests.get(url)
-    
+
     body = response.text
-    
+
     print("Body response:")
     print("\t- type:", type(body))
     print("\t- content:", body)
-
