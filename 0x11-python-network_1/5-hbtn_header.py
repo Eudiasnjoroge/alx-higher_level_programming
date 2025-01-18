b394bcfe523032b36a5deadd765412f3c391a475
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2025-01-18 12:51:55 +0300

    task 5 correction

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,9 +8,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    
+
     response = requests.get(url)
-    
+
     x_request_id = response.headers.get("X-Request-Id")
-    
+
     print(x_request_id)
